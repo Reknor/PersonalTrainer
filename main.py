af5f718be8672d96b6642cb7b99b6e335fbe04c4
Reknor <33392647+Reknor@users.noreply.github.com>
2021-05-20 00:46:03 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class PersonalTrainer(App):
+    pass
+
+
+def main():
+    PersonalTrainer().run()
+
+
+if __name__ == "__main__":
+    main()
